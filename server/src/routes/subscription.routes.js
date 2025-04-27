5da12e7d7b1b33283371dadf20fb05fe51e0861f
venkitotad <venkateshtotad9@gmail.com>
2025-04-27 20:09:52 +0530

    modified folder structure

---
+++ server/src/routes/subscription.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', getAllSubscription)
+subscriptionRouter.get('/', getAllSubscription)
+subscriptionRouter.post('/add-subscription', createSubscription)
+subscriptionRouter.post('/add-subscription', createSubscription)
+
